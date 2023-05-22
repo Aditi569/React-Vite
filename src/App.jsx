1700e2e6ba33c9fb8d81893b29b3e88b2fc951d9
aditi <aditirauniyar243@gmail.com>
2023-05-23 01:35:31 +0530

    made react counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import './App.css'
-// import {useState} from 'react'
-
+import { useState } from 'react'
 
 function App() {
- 
+
+  const [count, setCount] = useState(0)
+
+  // count = 1  in js we change varibale like this
+  // setCount(1) in react we use like this as func. argument
 
   return (
-    <>
-        <h1>Hello Aditi</h1>
-    </>
+
+
+
+    <div className='main-container'>
+      <div className="local-container">
+        <h1>{count}</h1>
+        {/* in onclick we can directly pass callback func. */}
+        <div className='button-container'>
+          <button className='btn-count' onClick={() => { setCount(count - 1) }}>-</button>
+          <button className='btn-count' onClick={() => { setCount(count + 1) }}>+</button>
+        </div>
+      </div>
+    </div>
+
   )
 }
 
